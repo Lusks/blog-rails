109ba4e3d0daca266abead84f1b8fafee40fb896
lusks <lucasnovaesprog@hotmail.com>
2022-09-19 21:29:17 -0300

    Created a controller, model, partials and added final adjustments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+
+	def create
+		@post = Post.find(params[:post_id])
+	 	@comment = @post.comments.create(params[:comment].permit(:name, :comment))
+		redirect_to post_path(@post)	
+	end
+
+	# The method below should be called "destroy", but rails won't let It run for some reason. 
+	# Probably because I was basing myself on an older video where the instructor wasn't
+	# clear about the rails version. It ended up originating this bug, but I made It work by
+	# changing It's name to "show".
+	
+	def show
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
+		redirect_to post_path(@post)
+	end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_165239) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_19_021304) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
